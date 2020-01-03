0bd796b82bfe181d6e4571af81248dec3da13ef2
dargaCode <darga.code@gmail.com>
2020-01-03 05:03:33 -0800

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js)$/,
+        use: "babel-loader"
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
       },
