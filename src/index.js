6d4bfc39a0a4e6aeea6206290ac56ea1b60d1a43
dargaCode <darga.code@gmail.com>
2020-01-03 03:41:27 -0800

    Add initial webpack hello world

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { join } from "lodash";
+import "./style.css";
+import icon from "./icon.png";
+import printMe from "./print.js";
+
+function component() {
+  const element = document.createElement('div');
+  const button = document.createElement('button');
+
+  element.innerHTML = join(['Output', 'Management'], ' ');
+  element.classList.add('hello');
+
+  button.innerHTML = "Click me and check the console";
+  button.onclick = printMe;
+
+  const myIcon = new Image();
+  myIcon.src = icon;
+
+  element.appendChild(myIcon);
+  element.appendChild(button);
+
+  return element;
+}
+
+document.body.appendChild(component());
