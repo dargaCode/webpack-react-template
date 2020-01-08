c354ba294622087c9bbb81a683e316945910684d
dargaCode <darga.code@gmail.com>
2020-01-08 02:01:32 -0800

    fix remaining linter errors

--- src/index.js
+++ src/index.js
@@ -5,21 +5,18 @@ import "./style.css";
 import icon from "./icon.png";
 import printMe from "./print";
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    const message = join(["Hello World!"], " ");
+function App() {
+  const message = join(["Hello World!"], " ");
 
-    return (
-      <div>
-        <div className="hello">{message}</div>
-        <img src={icon} alt="a desert at night" />
-        <button type="button" onClick={printMe}>
-          Click me and check the console
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <div className="hello">{message}</div>
+      <img src={icon} alt="a desert at night" />
+      <button type="button" onClick={printMe}>
+        Click me and check the console
+      </button>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
