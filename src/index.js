e4749f3736121ab7e8b1097c4aa4b8631fef2b4b
dargaCode <darga.code@gmail.com>
2020-01-08 01:18:50 -0800

    check in garbage to bypass husky

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { join } from "lodash";
-import "./style.css";
-import icon from "./icon.png";
-import printMe from "./print.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { join } from 'lodash';
+import './style.css';
+import icon from           './icon.png';
+import printMe from './print.js';
 
-class App extends React.Component {
+class App extends
+    React.Component {
   render() {
-    const message = join(["Hello World!"], " ");
-
-
-
-
-
-
-
-
-
+    const            message = join(['Hello World!'],            ' ' )
 
 
 
@@ -25,12 +17,14 @@ class App extends React.Component {
 
     return (
                                                             <div>
-        <div className={"hello"}>{message}</div>
-        <img src={icon} alt={"a desert at night"} />
-        <button onClick={printMe}>Click me and check the console</button>
+        <div className={'hello'}>{message}</div>
+        <img src={icon} alt={'a desert at night'} />
+                                   <button onClick={printMe}>
+                                     {'Click me and check the console'}</button>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document
+    .getElementById('root'))
