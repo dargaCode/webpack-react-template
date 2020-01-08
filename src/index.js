b813fae6921a21d1df887c36faa664a9b96af295
dargaCode <darga.code@gmail.com>
2020-01-08 01:55:38 -0800

    woooooo auto-fixing errors on commit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { join } from 'lodash';
-import './style.css';
-import icon from           './icon.png';
-import printMe from './print.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import { join } from "lodash";
+import "./style.css";
+import icon from "./icon.png";
+import printMe from "./print";
 
-class App extends
-    React.Component {
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
   render() {
-    const            message = join(['Hello World!'],            ' ' )
-
-
-
-
-
+    const message = join(["Hello World!"], " ");
 
     return (
-                                                            <div>
-        <div className={'hello'}>{message}</div>
-        <img src={icon} alt={'a desert at night'} />
-                                   <button onClick={printMe}>
-                                     {'Click me and check the console'}</button>
+      <div>
+        <div className="hello">{message}</div>
+        <img src={icon} alt="a desert at night" />
+        <button type="button" onClick={printMe}>
+          Click me and check the console
+        </button>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document
-    .getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
