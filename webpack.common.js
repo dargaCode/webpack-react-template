5fbe51022a9ec08261835856ea78625870da61b6
dargaCode <darga.code@gmail.com>
2020-01-16 02:32:42 -0800

    Clean up loader rule syntax

--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        use: { loader: "ts-loader" }
+        use: "ts-loader"
       },
       {
         test: /\.(js|jsx)$/,
@@ -31,11 +31,11 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
+        use: "file-loader"
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
+        use: "file-loader"
       }
     ]
   },
