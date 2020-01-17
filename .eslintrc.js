6cc8c68fa7ff831c69e88b706c5c9e4bd37685b3
dargaCode <darga.code@gmail.com>
2020-01-17 15:11:22 -0800

    Integrate typescript with eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,45 @@
-module.export = {
-  "extends": ["airbnb", "prettier", "prettier/react"],
-  "plugins": ["react", "prettier"],
-  "rules": {
-    "react/jsx-filename-extension": [
-      1,
-      {
-        "extensions": [".js", "jsx"]
-      }
-    ],
+module.exports = {
+  extends: [
+    "airbnb",
+    "prettier",
+    "prettier/react",
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  plugins: ["react", "prettier"],
+  rules: {
     "prettier/prettier": "off",
+    "react/jsx-filename-extension": [2, { extensions: [".jsx", ".tsx"] }], // disallow jsx in js or ts files
     "max-len": ["error", 80],
     "import/no-extraneous-dependencies": [
       "error",
       {
-        "devDependencies": true
+        devDependencies: true
       }
     ]
   },
-  "env": {
-    "browser": true,
-    "jest": true
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
   },
-  "settings": {
+  env: {
+    browser: true,
+    jest: true
+  },
+  settings: {
     "import/resolver": {
-      "webpack": {
-        "config": "./webpack.common.js"
+      webpack: {
+        config: "./webpack.common.js"
       }
+    },
+    react: {
+      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
     }
   }
 };
