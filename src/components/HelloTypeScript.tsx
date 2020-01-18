2607458618e96c5ee07cd52360ce004b7b43a340
dargaCode <darga.code@gmail.com>
2020-01-18 03:58:10 -0800

    Clean up comments

--- src/components/HelloTypeScript.tsx
+++ src/components/HelloTypeScript.tsx
@@ -1,4 +1,3 @@
-// import * as React from "react";
 import React from "react";
 
 interface HelloProps {
@@ -6,8 +5,6 @@ interface HelloProps {
   framework: string;
 }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
 export default class HelloTypeScript extends React.Component<HelloProps, {}> {
   render() {
     return (
