c242f3f7cd75f88b6fcf6ff6ad60d4572e558db4
dargaCode <darga.code@gmail.com>
2020-01-29 01:44:34 -0800

    Extract spelling whitelist to separate file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const whitelistedWords = require("./spellcheck.whitelist.js");
+
 module.exports = {
   extends: [
     "airbnb",
@@ -36,69 +39,7 @@ module.exports = {
     "spellcheck/spell-checker": [
       2,
       {
-        skipWords: [
-          "api",
-          "arg",
-          "args",
-          "asc",
-          "async",
-          "attrs",
-          "backend",
-          "const",
-          "ctrl",
-          "curr",
-          "desc",
-          "dest",
-          "dev",
-          "devtool",
-          "dir",
-          "dirname",
-          "el",
-          "eslint",
-          "ev",
-          "falsy",
-          "filename",
-          "fn",
-          "html",
-          "http",
-          "https",
-          "href",
-          "img",
-          "jpg",
-          "js",
-          "jsx",
-          "init",
-          "lifecycle",
-          "linter",
-          "md",
-          "memoize",
-          "metadata",
-          "mixin",
-          "nav",
-          "noop",
-          "num",
-          "param",
-          "png",
-          "prev",
-          "renderer",
-          "runtime",
-          "sourcemap",
-          "scss",
-          "src",
-          "str",
-          "tmp",
-          "todo",
-          "transpile",
-          "truthy",
-          "tsx",
-          "ui",
-          "unhandled",
-          "unshift",
-          "unwatch",
-          "util",
-          "webpack",
-          "www"
-        ]
+        skipWords: whitelistedWords
       }
     ]
   },
