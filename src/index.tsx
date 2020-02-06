97fb384d656523195474454e41b9d82c7fd2e749
dargaCode <darga.code@gmail.com>
2020-02-06 09:22:35 -0800

    Add routable navigation bar

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./style.scss";
 import icon from "./icon.png";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
 import HelloWorld from "./components/HelloWorld";
 import HelloTypeScript from "./components/HelloTypeScript";
 
 function App(): JSX.Element {
   return (
-    <div>
-      <HelloWorld descriptor="Webpack" />
-      <HelloTypeScript compiler="TypeScript" framework="React" />
-      <img src={icon} alt="a desert at night" />
-    </div>
+    <Router>
+      <div>
+        <header>
+          <NavigationBar />
+        </header>
+        <Switch>
+          <Route path="/home">
+            <div>
+              <h1>Home</h1>
+            </div>
+          </Route>
+
+          <Route path="/about">
+            <div>
+              <h1>About</h1>
+            </div>
+          </Route>
+
+          <Route path="/">
+            <div>
+              <h1>Default Route</h1>
+            </div>
+          </Route>
+        </Switch>
+        <HelloWorld descriptor="Webpack" />
+        <HelloTypeScript compiler="TypeScript" framework="React" />
+        <img src={icon} alt="a desert at night" />
+      </div>
+    </Router>
   );
 }
 
