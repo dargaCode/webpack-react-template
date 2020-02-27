0ad122b3d1a9cd6b7b1983afa0b1112b0d4188fa
dargaCode <darga.code@gmail.com>
2020-02-27 01:27:17 -0800

    Use webpack.dev.js for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
+const path = require("path");
+const custom = require("../webpack.dev.js");
+
 module.exports = {
   stories: ["../src/**/*.stories.jsx"],
   addons: ["@storybook/addon-actions", "@storybook/addon-links"],
-  webpackFinal: async config => {
-    // do mutation to the config
 
-    return config;
+  webpackFinal: config => {
+    return {
+      ...config,
+      module: { ...config.module, rules: custom.module.rules }
+    };
   }
 };
