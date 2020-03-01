a724e7619aa58adb88a84468aded391c19751ed7
dargaCode <darga.code@gmail.com>
2020-03-01 01:24:16 -0800

    Change stories back to jsx extensions

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.stories.tsx"],
+  stories: ["../src/**/*.stories.jsx"],
   addons: [
     "@storybook/addon-actions/register",
     "@storybook/addon-links/register"
