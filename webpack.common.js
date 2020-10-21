1ea3696314cba5b2522cd01c683e34043a1493a9
dargaCode <darga.code@gmail.com>
2020-10-21 00:10:23 -0700

    Update webpack from portfolio

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const HtmlWebpackRootPlugin = require("html-webpack-root-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackRootPlugin = require("html-webpack-root-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MomentLocalesWebpackPlugin = require("moment-locales-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -15,7 +16,8 @@ module.exports = {
     filename: "[name].bundle.js"
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"]
+    // TODO add aliases here for /components/, /src/, to remove need for lots of /../
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".scss"]
   },
   module: {
     rules: [
@@ -29,15 +31,37 @@ module.exports = {
       },
       {
         test: /\.scss$/,
+        exclude: /\.module\.scss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: "file-loader"
+        test: /\.module\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]___[hash:base64:5]"
+              },
+              // replace kebab-case css class names with camelCase strings
+              // so they can keep their conventions in both css and js
+              localsConvention: "dashesOnly"
+            }
+          },
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|pdf|docx)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[hash:base64:5].[ext]"
+        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: "file-loader"
+        loader: "file-loader"
       }
     ]
   },
@@ -47,10 +71,11 @@ module.exports = {
       eslint: false
     }),
     new HtmlWebpackPlugin({
-      title: "Webpack/React Template",
+      title: "Darga Webpack Template",
       template: "src/template.html"
     }),
     new HtmlWebpackRootPlugin(),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new MomentLocalesWebpackPlugin()
   ]
 };
